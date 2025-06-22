5e2533b41a28d4b992d4e409e5273f7e71b3220a
Njokatem Comfort Mbeng <49481570+Mbengparis@users.noreply.github.com>
2025-06-23 01:38:42 +0200

    initial commit

---
+++ src/directory/directory.component.jsx
@@ -0,0 +1,17 @@
+import CategoryItem from '../category-item/category-item.compoment';
+
+import './directory.styles.scss';
+
+const Directory = ({categories}) => {
+
+   return (
+      <div className="categories-container">
+        {categories.map((category) =>  (
+           <CategoryItem key={category.id} category={category} /> 
+        ))
+       }
+      </div>
+   )
+}
+
+export default Directory;
