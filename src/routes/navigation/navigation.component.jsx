0d01699155add2b9236387e0605997a491246e10
Njokatem Comfort Mbeng <49481570+Mbengparis@users.noreply.github.com>
2025-06-23 21:15:51 +0200

    added new components and routes

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Link, Outlet } from "react-router";
+import { Fragment } from "react/jsx-runtime";
+import { ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+      <Fragment>
+         <div className="navigation">
+            <Link className="logo-container" to='/'> 
+               <CrwnLogo  className="logo" />
+            </Link>
+            <div className="links-container">
+               <Link className="nav-link" to='/shop'>
+                 SHOP
+               </Link>
+               <Link className="nav-link" to='/shop'>
+                 CONTACT
+               </Link>
+            </div>
+         </div>
+         <Outlet />
+      </Fragment>
+  )
+}
+
+export default Navigation;
